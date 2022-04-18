2ec18798b4504a4e29d5af75582fca9325a7fa48
Alex Nelson <alexander.nelson@nist.gov>
2022-04-18 17:37:40 -0400

    Reformat with Black

--- case_utils/bindings/autogen.py
+++ case_utils/bindings/autogen.py
@@ -39,13 +39,23 @@ def class_basename(class_iri: str) -> str:
 
 
 class CASEPropertyConstructor(object):
-    def __init__(self, property_iri: str, min_count: int = 0, max_count: typing.Optional[int] = None, *args, **kwargs) -> None:
+    def __init__(
+        self,
+        property_iri: str,
+        min_count: int = 0,
+        max_count: typing.Optional[int] = None,
+        *args,
+        **kwargs
+    ) -> None:
         self.property_iri: str = property_iri
         self.min_count: int = min_count
         self.max_count: typing.Optional[int] = max_count
 
+
 class CASEClassConstructor(object):
-    def __init__(self, class_iri: str, *args, documentation_comment: str = None, **kwargs) -> None:
+    def __init__(
+        self, class_iri: str, *args, documentation_comment: str = None, **kwargs
+    ) -> None:
         self.class_iri: str = class_iri
         self.case_class_name: str = "case_" + class_basename(class_iri)
         self.documentation_comment: typing.Optional[str] = documentation_comment
@@ -57,7 +67,10 @@ class CASEClassConstructor(object):
 
         parts.append(
             "class %s(%s):"
-            % (self.case_class_name, ", ".join(sorted(self.parent_constructor_class_names)))
+            % (
+                self.case_class_name,
+                ", ".join(sorted(self.parent_constructor_class_names)),
+            )
         )
 
         # Build class documentation string.
@@ -72,10 +85,12 @@ class CASEClassConstructor(object):
         parts.append('    """')
 
         # Build initializer.
-        parts.append("    def __init__(self, *args, type_iris: typing.Set[str] = set(), **kwargs) -> None:")
+        parts.append(
+            "    def __init__(self, *args, type_iris: typing.Set[str] = set(), **kwargs) -> None:"
+        )
         # Add types in initializer.
         if "'" in self.class_iri:
-            raise ValueError("Unexpected \"'\" character in IRI: %r." % self.class_iri)
+            raise ValueError('Unexpected "\'" character in IRI: %r.' % self.class_iri)
         parts.append("        if len(type_iris) == 0:")
         parts.append("            _type_iris = {'%s'}" % self.class_iri)
         parts.append("        else:")
@@ -92,14 +107,18 @@ class CASEClassConstructor(object):
         if self.case_class_name == "case_UcoObject":
             parts.append("    def add_facet(self, facet: case_Facet) -> None:")
             parts.append("        self.facets.append(facet)")
-            parts.append("        self.graph.add((self.node, NS_UCO_CORE.hasFacet, facet.node))")
+            parts.append(
+                "        self.graph.add((self.node, NS_UCO_CORE.hasFacet, facet.node))"
+            )
             parts.append("    @property")
             parts.append("    def facets(self) -> typing.List[case_Facet]:")
             parts.append("        return self._facets")
         elif self.case_class_name == "case_ContentDataFacet":
             parts.append("    def add_hash(self, hash: case_Hash) -> None:")
             parts.append("        self.hashes.append(hash)")
-            parts.append("        self.graph.add((self.node, NS_UCO_OBSERVABLE.hash, hash.node))")
+            parts.append(
+                "        self.graph.add((self.node, NS_UCO_OBSERVABLE.hash, hash.node))"
+            )
             parts.append("    @property")
             parts.append("    def hashes(self) -> typing.List[case_Hash]:")
             parts.append("        return self._hashes")
@@ -207,7 +226,7 @@ class NodeConstructor(object):
     )
 
     class_iris: typing.Set[str] = set()
-    class_iri_to_documentation_comment: typing.Dict[str, typing.Optional[str]] = dict() 
+    class_iri_to_documentation_comment: typing.Dict[str, typing.Optional[str]] = dict()
     class_query_str = """\
 SELECT ?nClass ?nComment
 WHERE {
@@ -220,7 +239,9 @@ WHERE {
     for class_result in graph.query(class_query_str):
         class_iris.add(class_result[0].toPython())
         if not class_result[1] is None:
-            class_iri_to_documentation_comment[class_result[0].toPython()] = class_result[1].toPython()
+            class_iri_to_documentation_comment[
+                class_result[0].toPython()
+            ] = class_result[1].toPython()
 
     constructor_class_name_to_case_class_constructor: typing.Dict[
         str, CASEClassConstructor
@@ -234,7 +255,10 @@ WHERE {
             raise ValueError("Duplicate class name: %r." % constructor_class_name)
         constructor_class_name_to_case_class_constructor[
             constructor_class_name
-        ] = CASEClassConstructor(class_iri, documentation_comment=class_iri_to_documentation_comment.get(class_iri))
+        ] = CASEClassConstructor(
+            class_iri,
+            documentation_comment=class_iri_to_documentation_comment.get(class_iri),
+        )
 
     # Link generated class names.
     constructor_class_dependencies = networkx.DiGraph()
@@ -263,15 +287,17 @@ WHERE {
         constructor_class_name_to_case_class_constructor[
             constructor_class_name
         ].parent_constructor_class_names.add(parent_constructor_class_name)
-        constructor_class_dependencies.add_edge(constructor_class_name, parent_constructor_class_name)
+        constructor_class_dependencies.add_edge(
+            constructor_class_name, parent_constructor_class_name
+        )
 
     # TODO Record properties.
 
     # Serialize classes in topological order of subclass hierarchy.
     # Near-one-liner for topological sort c/o: https://stackoverflow.com/a/56476639
-    for constructor_class_name in reversed(list(
-        networkx.topological_sort(constructor_class_dependencies)
-    )):
+    for constructor_class_name in reversed(
+        list(networkx.topological_sort(constructor_class_dependencies))
+    ):
         if constructor_class_name == "NodeConstructor":
             # Skip the one hand-written class.
             continue
