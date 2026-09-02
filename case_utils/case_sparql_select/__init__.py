501c5a4ea6d79e0c1c328880c6dfde231b306e1c
Alex Nelson <alexander.nelson@nist.gov>
2026-09-02 11:13:56 -0400

    Bump versions
    No effects were observed on Make-managed files.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- case_utils/case_sparql_select/__init__.py
+++ case_utils/case_sparql_select/__init__.py
@@ -29,7 +29,7 @@ The word "DISTINCT" will also be cut from the query, if present.
 Should a more complex query be necessary, an outer, wrapping SELECT query would let this script continue to function.
 """
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 import argparse
 import binascii
--- case_utils/case_validate/validate_utils.py
+++ case_utils/case_validate/validate_utils.py
@@ -14,7 +14,7 @@
 #
 # We would appreciate acknowledgement if the software is used.
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 import importlib
 import logging
